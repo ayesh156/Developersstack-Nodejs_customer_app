7b8895ac5eb68368baf561c5f618efb4af0cbecd
= <=>
2024-06-27 14:28:12 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+
+const customerRoute = require('./routes/customerRoute');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/api/v1/admin/customers',customerRoute);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found!</h1>');
+});
+
+const port = process.env.SERVER_PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Server Started & running on port ${port}`);
+})
